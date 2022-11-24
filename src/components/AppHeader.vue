56d89567fa398978373ab62d8d135ba1a4b0b50d
Seme87 <massimomonteleone@hotmail.com>
2022-11-24 17:18:03 +0100

    aggiunge struttura header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,13 +9,51 @@ export default {
 
 <template>
     <header class="container">
-        <h1>Header</h1>
+        <div class="logo">
+            <img src="../assets/dc-logo.png" alt="Logo-header">
+        </div>
+        <nav class="main-nav">
+          <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLUES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul>
+        </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
 
-
+header{
+  padding: 20px 10px;
+  display: flex;
+  justify-content: space-between;
+  .main-nav{
+    display: flex;
+    align-items: center;
+    ul{
+      display: flex;
+      list-style: none;
+      align-items: center;
+      a{
+        font-size: 13px;
+        height: 100%;
+        font-weight: bold;
+        display: inline-block;
+        padding: 0 15px;
+        text-decoration: none;
+        color: #000;
+      }
+    }
+  }
+}
 
 // header {
 //     text-align: center;
